8535d3746463739a19567d00f5fda04025bc565e
Ben Rodenhäuser <ben@karo.berlin>
2017-07-25 18:35:18 +0200

    RPS

--- lesson_02/rock_paper_scissors.rb
+++ lesson_02/rock_paper_scissors.rb
@@ -1,54 +1,122 @@
-class Player
-  def choose
-    @move = Move.new
-  end
-end
-
 class Move
-  CHOICES = [:rock, :paper, :scissors]
+  VALUES = ['rock', 'paper', 'scissors']
 
-  def initialize
-    @choice = CHOICES.sample
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    other_move > self
+  end
+
+  def to_s
+    value
   end
 end
 
-class Rule
+class Player
+  attr_accessor :move
+
   def initialize
-    # not sure what the "state" of a rule object should be
+    @move = nil
   end
 end
 
-# not sure where "compare" goes yet
-def compare(move1, move2)
+class Human < Player
+  def choose
+    choice = nil
+    loop do
+      puts "please make your choice among rock, paper, scissors"
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+      puts "invalid choice"
+    end
+    self.move = Move.new(choice)
+  end
+end
 
+class Computer < Player
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
 end
 
 class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+    loop do
+      human.choose
+      computer.choose
+      display_moves
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 
+  def play_again?
+    answer = nil
+    loop do
+      puts "do you want to play again?"
+      answer = gets.chomp
+      break if answer.downcase.start_with?('y', 'n')
+      puts "invalid choice"
+    end
+
+    answer.start_with?('y')
+  end
+
   def display_welcome_message
-    puts "hello!"
+    puts "welcome to rock, paper, scissors!"
   end
 
   def display_goodbye_message
-    puts "goodbye!"
+    puts "thanks for playing rock, paper, scissors! goodbye!"
+  end
+
+  def display_moves
+    puts "you chose #{human.move}"
+    puts "computer chose #{computer.move}"
   end
 
   def display_winner
-    # todo
+    result =
+      if human.move > computer.move
+        "you won"
+      elsif human.move < computer.move
+        "the computer won"
+      else
+        "it's a tie."
+      end
+    puts result
   end
-
 end
+
+RPSGame.new.play
