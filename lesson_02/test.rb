4464281b6b02787a235dab30e511cf8fdd528479
Ben Rodenhäuser <ben@karo.berlin>
2017-07-28 11:38:22 +0200

    minor

--- lesson_02/test.rb
+++
@@ -1,8 +0,0 @@
-def determine_winner
-  # implementation omitted.
-  # returns the name of the winner as a string if there is a winner, `nil` otherwise.
-  nil
-end
-
-winner = determine_winner
-puts winner || "no winner has been found"
